6e9c516313b1d7bdf2b1dcc542cda9b71e620c23
John Bass <John_Bass2@cable.comcast.com>
2016-04-25 17:14:20 -0700

    Added zookeeper installation script

---
+++ install-zookeeper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "####################################################################"
+echo "##        Installing internal Zookeeper node for testing          ##"
+echo "####################################################################"
+wget -N http://apache.claz.org/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz
+tar -xvzf ./zookeeper-3.4.6.tar.gz
+
