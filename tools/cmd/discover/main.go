578bbc7299520cf1a4cabb84484da5d101764831
John Bass <John_Bass2@cable.comcast.com>
2016-04-28 16:53:48 -0700

    Fixed the double Sprintf in the default logger; fleshed out the discover tool

--- tools/cmd/discover/main.go
+++ tools/cmd/discover/main.go
@@ -6,7 +6,14 @@ import (
 	"github.com/Comcast/golang-discovery-client/service"
 	"github.com/Comcast/golang-discovery-client/service/cmd"
 	"os"
+	"os/signal"
 )
+
+func ServiceListener(logger service.Logger) service.Listener {
+	return service.ListenerFunc(func(serviceName string, instances service.Instances) {
+		logger.Info("Updated [%s] services: %s", serviceName, instances)
+	})
+}
 
 func main() {
 	cmd.ConfigureCommandLine()
@@ -14,11 +21,25 @@ func main() {
 	discoveryBuilder := &service.DiscoveryBuilder{}
 	cmd.ApplyCommandLine(discoveryBuilder)
 
-	discovery, err := discoveryBuilder.NewDiscovery(&service.DefaultLogger{os.Stdout}, true)
+	logger := &service.DefaultLogger{os.Stdout}
+	discovery, err := discoveryBuilder.NewDiscovery(logger, true)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to start discovery client: %v\n", err)
 		os.Exit(1)
 	}
 
-	defer discovery.Close()
+	for _, serviceName := range discovery.ServiceNames() {
+		if instances, err := discovery.FetchServices(serviceName); err != nil {
+			logger.Error("Unable to fetch initial [%s] services: %v", serviceName, err)
+		} else {
+			logger.Info("Initial [%s] services: %s", serviceName, instances)
+		}
+	}
+
+	discovery.AddListenerForAll(ServiceListener(logger))
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	<-signals
+	discovery.Close()
 }
