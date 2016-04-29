52509484ca4f181bb6d344f3fcbbbc7ee9557802
John Bass <John_Bass2@cable.comcast.com>
2016-04-28 18:02:58 -0700

    Pretty output in response to watch events

--- tools/cmd/discover/main.go
+++ tools/cmd/discover/main.go
@@ -4,14 +4,69 @@ import (
 	"flag"
 	"github.com/Comcast/golang-discovery-client/service"
 	"github.com/Comcast/golang-discovery-client/service/cmd"
+	"github.com/foursquare/fsgo/net/discovery"
 	"os"
 	"os/signal"
+	"strconv"
 )
 
+func printService(logger service.Logger, status string, instance *discovery.ServiceInstance) {
+	port := "?"
+	portType := "?"
+	if instance.Port != nil {
+		port = strconv.Itoa(*instance.Port)
+		portType = "http"
+	} else if instance.SslPort != nil {
+		port = strconv.Itoa(*instance.SslPort)
+		portType = "https"
+	}
+
+	logger.Info(
+		"\t%-8.8s | %-37.37s | %-5.5s | %-32.32s | %-5.5s",
+		status,
+		instance.Id,
+		portType,
+		instance.Address,
+		port,
+	)
+}
+
 func printServices(logger service.Logger, serviceName string, oldInstances, newInstances service.Instances) {
-	logger.Info("\t")
-	logger.Info("\tService: %s", serviceName)
-	logger.Info("\t")
+	logger.Info("\t##### %s #####", serviceName)
+
+	oldInstancesById := make(map[string]*discovery.ServiceInstance, len(oldInstances))
+	newInstancesById := make(map[string]*discovery.ServiceInstance, len(newInstances))
+
+	for _, instance := range oldInstances {
+		oldInstancesById[instance.Id] = instance
+	}
+
+	for _, instance := range newInstances {
+		newInstancesById[instance.Id] = instance
+	}
+
+	for id, instance := range oldInstancesById {
+		if _, ok := newInstancesById[id]; ok {
+			// this is an existing instance
+			delete(newInstancesById, id)
+			printService(logger, "EXISTING", instance)
+		} else {
+			// this instance was removed
+			printService(logger, "REMOVED", instance)
+		}
+	}
+
+	for _, instance := range newInstancesById {
+		// each of the remaining entries is a new instance
+		printService(logger, "NEW", instance)
+	}
+
+	logger.Info(
+		"\tSummary:  There were %d %s service(s).  There are now %d service(s).",
+		len(oldInstances),
+		serviceName,
+		len(newInstances),
+	)
 }
 
 func initialServices(logger service.Logger, discovery service.Discovery) map[string]service.Instances {
